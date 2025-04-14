5af7dd973728eea20cea6ecbab5d13a86f039e74
Shibam57 <shibamsamantascience@gmail.com>
2025-04-14 23:20:14 +0530

    react project

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti distinctio suscipit, a, voluptatum obcaecati tenetur inventore maiores odit, dolore numquam sint? Tempore aliquid aspernatur similique reiciendis ea aliquam accusamus laudantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit quis incidunt placeat dicta nemo explicabo, excepturi quam vitae totam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus corporis quis hic perferendis tempore! Quia nisi natus animi at possimus!</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
