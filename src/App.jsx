acc19f2514a20469990e4cd2c65f252001156182
Shibam57 <shibamsamantascience@gmail.com>
2025-08-21 14:59:01 +0530

    hdkf

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
           <Route path="/product" element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
-          <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
